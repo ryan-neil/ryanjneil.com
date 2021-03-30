f36008beb21cb9a4bdb1cc13acd34e361c5dfce7
Ryan Neil <ryanjosephneil@gmail.com>
2021-03-29 16:30:06 -1000

    add: dark mode

--- dist-dark/components/6 - about-page/js/app.js
+++
@@ -1,8 +0,0 @@
-// Start Navbar transition on scroll
-window.addEventListener('scroll', function () {
-  let header = document.querySelector('header');
-  let windowPosition = window.scrollY > 75;
-
-  header.classList.toggle('scrolling-active', windowPosition);
-})
-// End Navbar transition on scroll
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,20 +1,62 @@
+// Start Button Dark and Light Mode
+// users' last choice will be remembered
+
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+// check if dark mode is enabled
+// if it's enabled, turn it off
+// if it's disabled, turn it on
+
+const enableDarkMode = () => {
+	// 1. add the class of darkMode to the body
+	document.body.classList.add("darkmode");
+	// 2. update darkMode in the localStorage
+	localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+	// 1. remove the class of darkMode from the body
+	document.body.classList.remove("darkmode");
+	// 2. update darkMode in the localStorage
+	localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+	enableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+	// update darkMode every time someone clicks on the button
+	darkMode = localStorage.getItem("darkMode");
+	if (darkMode !== "enabled") {
+		enableDarkMode();
+		console.log(darkMode);
+	} else {
+		disableDarkMode();
+		console.log(darkMode);
+	}
+});
+
+// End Button Dark and Light Mode
+
 // Start Navbar transition on scroll
-window.addEventListener('scroll', function() {
-	let header = document.querySelector('header');
+window.addEventListener("scroll", function() {
+	let header = document.querySelector("header");
 	let windowPosition = window.scrollY > 75;
 
-	header.classList.toggle('scrolling-active', windowPosition);
+	header.classList.toggle("scrolling-active", windowPosition);
 });
 // End Navbar transition on scroll
 
 // Start Site Faders
-const faders = document.querySelectorAll('.fade-in');
-const sliders = document.querySelectorAll('.slide-in');
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
 
 // faders
 const appearOptions = {
 	threshold  : 0,
-	rootMargin : '0px 0px -100px 0px'
+	rootMargin : "0px 0px -100px 0px"
 };
 
 const appearOnScroll = new IntersectionObserver(function(
@@ -25,7 +67,7 @@ const appearOnScroll = new IntersectionObserver(function(
 		if (!entry.isIntersecting) {
 			return;
 		} else {
-			entry.target.classList.add('appear');
+			entry.target.classList.add("appear");
 			appearOnScroll.unobserve(entry.target);
 		}
 	});
